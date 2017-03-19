6beac805b6bf54924f6b9ef22e44ab09c9167294
Maryna <Maryna@Marina-Book>
2017-03-19 12:18:12 +0200

    new project

--- Schedule/src/schedule/Account.java
+++ Schedule/src/schedule/Account.java
@@ -1,14 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package schedule;
+import java.util.*;
 
-/**
- *
- * @author Maryna
- */
-public class Account {
+
+    public class Account {
+        private String name; //поле потому что написано не в методе а в классе
+        private String login;
+        private String password;
+        private long dateLastLogging;
+        private String type;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public long getDateLastLogging() {
+        return dateLastLogging;
+    }
+
+    public void setDateLastLogging(long dateLastLogging) {
+        this.dateLastLogging = dateLastLogging;
+    }
     
+    public String getType () {
+        return this.type;
+    }
+        
+    public void setType(String type){
+        if (type == "student" || type == "teacher"){
+            this.type = type;
+        }
+    }
 }
