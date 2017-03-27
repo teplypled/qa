b7003b6f60dc8d8035912e8fb69eb34bc48340ca
Maryna <Maryna@Marina-Book>
2017-03-27 21:29:17 +0300

    селеніум початок

--- TestSelenium/src/testselenium/TestSel.java
+++ TestSelenium/src/testselenium/TestSel.java
@@ -5,11 +5,14 @@
  */
 package testselenium;
 
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import static org.openqa.grid.common.SeleniumProtocol.WebDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.internal.ProfilesIni;
 
 /**
  *
@@ -17,16 +20,35 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  */
 public class TestSel {
     
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException{
         System.setProperty("webdriver.gecko.driver", "C:\\selenium\\geckodriver.exe");
         
-        WebDriver webdr = new FirefoxDriver();
-        try {
-            Thread.sleep(20*1000);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(TestSel.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        ProfilesIni profiles = new ProfilesIni();
         
+        
+        FirefoxProfile profile = profiles.getProfile("WDS");
+        profile.setPreference("permissions.default.image", 2); // заборона показувати картинки 2 -не показують, 1 - показують
+
+        profile.setPreference("network.proxy.type", 1);
+        profile.setPreference("network.proxy.type", "121.122.123.2");
+        profile.setPreference("network.proxy.type", 8502);
+        
+        WebDriver webdr = new FirefoxDriver(profile); // можна прибоати profile якщо невідомий
+        webdr.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS); //очікування загрузки сторінки
+        webdr.manage().timeouts().setScriptTimeout(30, TimeUnit.SECONDS); //очікування загрузки скрипта
+     
+        
+        webdr.get("http://www.ukr.net");
+        WebElement someElement = webdr.findElement(by)// не дописано рядок!!!!!!!!!!!!!!!!!!
+        String link = someElement.getAttribute("href");
+        webdr.navigate().to(link);
+        someElement.click();
+        Thread.sleep(1000*20);
+        
+        
+        
+        Thread.sleep(20*1000);
+               
         
         webdr.quit();
         
